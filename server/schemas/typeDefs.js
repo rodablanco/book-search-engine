10eb32c6d26a3ee635933a5ddb040b285b0046d2
Rod <rodrigo.a.blanco1990@gmail.com>
2021-08-10 08:22:26 -0700

    making basic set up fro files

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,23 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = ggl`
+type Me {
+    username: String!
+}
+
+type Mutation{
+    
+}
+
+type User {
+
+}
+
+type Book {
+
+}
+
+type Auth{
+
+}
+`
---
+++ server/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const path = require('path');
+const { ApolloServer } = require('apolo-server-express');
+const db = require('./config/connection');
+// const routes = require('./routes');
+const { typeDefs, resolvers } = require('./schemas');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+})
+
+server.applyMiddleware({ app });
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.length('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+})
+// app.use(routes);
+
+db.once('open', () => {
+  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+   // log where we can go to test our GQL API
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`)
+});
