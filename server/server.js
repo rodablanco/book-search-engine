ef88cd835fc06f2026678bda1dd36933491151d8
Rod <rodrigo.a.blanco1990@gmail.com>
2021-08-11 11:06:56 -0700

    deploying

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const db = require('./config/connection');
 //importing schemas
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
-const db = require('./config/connection');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
