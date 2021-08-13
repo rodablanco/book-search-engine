eed4e07cf4cb73a40c4f65265ea0b0ecf62f1ffc
Rod <rodrigo.a.blanco1990@gmail.com>
2021-08-12 21:29:55 -0700

    fixing errors throughout docs

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -17,7 +17,7 @@ mutation login($email: String!, $password: String!){
 
 //adding user mutation
 export const ADD_USER = gql `
-mutation addUser($username: String!, email: String!, password: String!) {
+mutation addUser($username: String!, $email: String!, $password: String!) {
     addUser(username: $username, email: $email, password: $password) {
         token
         user {
